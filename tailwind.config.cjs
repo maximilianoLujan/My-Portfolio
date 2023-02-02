3f7ffec9beaf27fbc77ce983930b3f900a8ba1d4
Maximiliano <maxylujan1@outlook.com>
2023-02-02 11:22:32 -0300

    Realizando la seccion de portada

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+      xs:'368px',
+      ...defaultTheme.screens,
+    },
     extend: {},
     colors: {
       'greyHero':'#63676A',
       'textHero':'#EBEBEC',
       'blackNavbar':'#1D1A19',
       'black':'#000000',
+      'bg-social':'#C4C4C4'
     },
   },
   plugins: [],
